ec59db3f77a6bb997ba58823a7a6b5357cf60d18
[Tim Chimfwembe] <78848435+OluwaMukulu@users.noreply.github.com>
2023-10-21 12:26:27 +0200

    static2

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -123,8 +123,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = '/var/www/DjangoApp/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
